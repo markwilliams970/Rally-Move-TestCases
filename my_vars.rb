bbae80b376624e9c530d2c2096175597445d06d9
markwilliams <markwilliams@rallydev.com>
2013-05-09 15:09:45 -0600

    User-specific environment variables

---
+++ my_vars.rb
@@ -0,0 +1,7 @@
+$my_base_url       = "https://rally1.rallydev.com/slm"
+$my_username       = "user@company.com"
+$my_password       = "password"
+$my_workspace      = "My Workspace"
+$my_project        = "My Project"
+$wsapi_version     = "1.40"
+$filename          = 'move_test_cases.csv'
